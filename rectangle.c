8e77fa05e664d3e2af32a997291fb76ed13d4763
Ishan Kakad <97300918+IshanKakad@users.noreply.github.com>
2022-12-13 23:03:58 +0530

    Create rectangle.c

---
+++ rectangle.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+float length, breadth;
+    printf("\n\n #FOR RECTANGLE#\n");
+    printf("enter length:");
+    scanf("%f", &length);
+    printf("enter breadth:");
+    scanf("%f", &breadth);
+    printf("AREA=%f",length * breadth);
+    printf("\nCIRCUMFERENCE=%f",2 * (length + breadth));
