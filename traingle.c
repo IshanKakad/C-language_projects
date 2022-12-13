5cca601d02ce0bfe907d9d72e7068d67e4919b61
Ishan Kakad <97300918+IshanKakad@users.noreply.github.com>
2022-12-13 23:04:26 +0530

    Create traingle.c

---
+++ traingle.c
@@ -0,0 +1,12 @@
+float side1, side2, side3, s , m;
+    printf("\n \n#FOR TRIANGLE#\n");
+    printf("enter first side of triangle: ");
+    scanf("%f", &side1);
+    printf("enter second side of triangle: ");
+    scanf("%f", &side2);
+    printf("enter third side of triangle: ");
+    scanf("%f", &side3);
+    s = (side1 + side2 + side3)/2;
+    m = sqrt (s * (s-side1)*(s-side2)*(s-side3));
+    printf("AREA=%f", m);
+    printf("\ncircumference=%f", side1 + side2 + side3);
