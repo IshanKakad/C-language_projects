50d31e8fa61abacc328ba8859627c5f1c9826df3
Ishan Kakad <97300918+IshanKakad@users.noreply.github.com>
2023-01-15 15:24:07 +0530

    Update and rename armstrongnumber.c to 3digitarmstrongnumber.c

--- 3digitarmstrongnumber.c
+++ 3digitarmstrongnumber.c
@@ -1,24 +1,18 @@
 #include <stdio.h>
-#include <math.h>
-
 int main() {
-    int num, originalNum, remainder, n = 0, result = 0;
-
-    printf("Enter an integer: ");
+    int num, originalNum, remainder, result = 0;
+    printf("Enter a three-digit integer: ");
     scanf("%d", &num);
     originalNum = num;
 
     while (originalNum != 0) {
-        originalNum /= 10;
-        ++n;
-    }
-
-    originalNum = num;
-
-    while (originalNum != 0) {
+       // remainder contains the last digit
         remainder = originalNum % 10;
-        result += pow(remainder, n);
-        originalNum /= 10;
+        
+       result += remainder * remainder * remainder;
+        
+       // removing last digit from the orignal number
+       originalNum /= 10;
     }
 
     if (result == num)
